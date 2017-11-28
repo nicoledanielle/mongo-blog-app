ef3d4fcf0bcc79d1f8544bbbc3b1a31158d39505
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-11-28 14:33:36 -0800

    installed body parser

--- models.js
+++ models.js
@@ -10,7 +10,7 @@ const postSchema = mongoose.Schema({
     lastName: String
   },
   content: {type: String, required: true},
-  publishDate: Date.now()
+  created: {type: Date, default: Date.now}
 });
 
 //virtuals
@@ -21,76 +21,14 @@ postSchema.virtual('authorName').get(function() {
 postSchema.methods.apiRepr = function () {
   
   return{
+    id: this._id,
     title: this.title,
     author: this.authorName,
     content: this.content,
-    publishDate: this.publishDate
+    created: this.created
   };
 };
 
 const Post = mongoose.model('Post', postSchema);
 
 module.exports = {Post};
-
-
-// const uuid = require('uuid');
-
-
-// function StorageException(message) {
-//   this.message = message;
-//   this.name = 'StorageException';
-// }
-
-// const BlogPosts = {
-//   create: function(title, content, author, publishDate) {
-//     const post = {
-//       id: uuid.v4(),
-//       title: title,
-//       content: content,
-//       author: author,
-//       publishDate: publishDate || Date.now()
-//     };
-//     this.posts.push(post);
-//     return post;
-//   },
-//   get: function(id=null) {
-//     // if id passed in, retrieve single post,
-//     // otherwise send all posts.
-//     if (id !== null) {
-//       return this.posts.find(post => post.id === id);
-//     }
-//     // return posts sorted (descending) by
-//     // publish date
-//     return this.posts.sort(function(a, b) {
-//       return b.publishDate - a.publishDate;
-//     });
-//   },
-//   delete: function(id) {
-//     const postIndex = this.posts.findIndex(
-//       post => post.id === id);
-//     if (postIndex > -1) {
-//       this.posts.splice(postIndex, 1);
-//     }
-//   },
-//   update: function(updatedPost) {
-//     const {id} = updatedPost;
-//     const postIndex = this.posts.findIndex(
-//       post => post.id === updatedPost.id);
-//     if (postIndex === -1) {
-//       throw StorageException(
-//         `Can't update item \`${id}\` because doesn't exist.`);
-//     }
-//     this.posts[postIndex] = Object.assign(
-//       this.posts[postIndex], updatedPost);
-//     return this.posts[postIndex];
-//   }
-// };
-
-// function createBlogPostsModel() {
-//   const storage = Object.create(BlogPosts);
-//   storage.posts = [];
-//   return storage;
-// }
-
-
-// module.exports = {BlogPosts: createBlogPostsModel()};
